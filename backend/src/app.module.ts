6ef541d18bd5bc4094a308aaa078366b48c92858
Alejo Borracci <borraccialejo@hotmail.es>
2024-11-20 09:37:44 -0300

    modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { MatchesModule } from './matches/matches.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [MatchesModule],
+  imports: [MatchesModule, UsersModule],
 })
 export class AppModule {}
