be29c9d4c17b5776f93dad6edc6d58fced75b448
Alejo Borracci <borraccialejo@hotmail.es>
2024-11-26 14:03:04 -0300

    env

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,18 +2,22 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { GamesModule } from './games/games.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+require('dotenv').config();
+
 
 
 @Module({
   imports: [UsersModule, GamesModule,TypeOrmModule.forRoot({
     type: 'postgres',               
-    host: 'localhost',              
+    host: "localhost",              
     port: 5433,                      
-    username: 'postgres',          
-    password: '29demarzo',       
-    database: 'postgres',   
+    username: "postgres",          
+    password: "29demarzo",       
+    database: "postgres",   
     entities: [__dirname + '/**/*.entity{.ts,.js}'], 
     synchronize: true,               
   }),],
-})
+},)
+
+
 export class AppModule {}
