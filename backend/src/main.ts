f5f481f5863bc20ee77e5ed418abbf1488d03dd1
Alejo Borracci <borraccialejo@hotmail.es>
2024-12-02 15:22:18 -0300

    cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://localhost:3001', 'https://scrims-rank-6qkx8pj8x-alejoborracci21s-projects.vercel.app/'], // Permitir solicitudes desde el frontend
+    origin: 'https://scrims-rank-2s2gagtiw-alejoborracci21s-projects.vercel.app/', // Permitir solicitudes desde el frontend
     methods: 'GET,POST,PUT,DELETE,OPTIONS', // Métodos HTTP permitidos
     credentials: true, // Permitir cookies o encabezados con credenciales
   });
