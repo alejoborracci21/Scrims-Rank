b578da92f518121b26216998169d7e561efdc609
Alejo Borracci <borraccialejo@hotmail.es>
2024-12-02 13:30:32 -0300

    deploy

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,22 +2,28 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { GamesModule } from './games/games.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-require('dotenv').config();
+import * as dotenv from 'dotenv';
 
-
+dotenv.config();
 
 @Module({
-  imports: [UsersModule, GamesModule,TypeOrmModule.forRoot({
-    type: 'postgres',               
-    host: "localhost",              
-    port: 5433,                      
-    username: "postgres",          
-    password: "29demarzo",       
-    database: "postgres",   
-    entities: [__dirname + '/**/*.entity{.ts,.js}'], 
-    synchronize: true,               
-  }),],
-},)
-
-
+  imports: [
+    UsersModule,
+    GamesModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+      ssl: { rejectUnauthorized: false }, 
+      extra: {
+        max: 5, // Reduce el número máximo de conexiones simultáneas
+      },
+    }),
+  ],
+})
 export class AppModule {}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.enableCors({
+    origin: 'http://localhost:3001', // Permitir solicitudes desde el frontend
+    methods: 'GET,POST,PUT,DELETE,OPTIONS', // Métodos HTTP permitidos
+    credentials: true, // Permitir cookies o encabezados con credenciales
+  });
+  
+  await app.listen(process.env.DB_PORT ?? 3000);
+
 }
 bootstrap();
