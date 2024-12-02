0979da57312482db81f90b9f883a727dbf6bd72f
Alejo Borracci <borraccialejo@hotmail.es>
2024-12-02 14:01:07 -0300

    url

--- frontend/src/app/components/login.tsx
+++ frontend/src/app/components/login.tsx
@@ -18,7 +18,7 @@ const Login = () => {
     const user = { email, password };
 
     try {
-      const response = await fetch('http://localhost:3000/users/login', {
+      const response = await fetch('https://scrims-rank.onrender.com/users/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
