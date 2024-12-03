b4d0fbbc1d092e6447d54a5e8eabaa3488a20aad
Alejo Borracci <borraccialejo@hotmail.es>
2024-12-03 12:15:58 -0300

    login

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,14 +1,10 @@
 'use client'
 
 import Login from "./components/login";
-import Head from "next/head";  // Importa el componente Head
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <meta name="description" content="Bienvenido a nuestra página de inicio." />
-      </Head>
       <Login />
     </>
   );
