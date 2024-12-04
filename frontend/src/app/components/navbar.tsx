5b009257b473473aa8ccb739fff7a7ed0824c89a
Alejo Borracci <borraccialejo@hotmail.es>
2024-12-04 16:18:38 -0300

    styles

--- frontend/src/app/components/navbar.tsx
+++ frontend/src/app/components/navbar.tsx
@@ -3,17 +3,62 @@ import * as React from 'react';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import { useRouter } from 'next/navigation'; // Usar el nuevo router del App Router
+import background from "@/../public/navbar.webp"; // Asegúrate de que la ruta sea correcta
 
 const Navbar = () => {
   const router = useRouter();
 
   return (
-    <div className="flex flex-row justify-center bg-[#f12727] w-[100vw] h-[7vh] fixed text-white shadow-xl shadow-black">
+    <div className="flex flex-row justify-center w-[100vw] h-[7vh] fixed text-white shadow-xl shadow-black" style={{
+      backgroundImage: `url(${background.src})`,
+      backgroundSize: "cover",
+      backgroundPosition: "center",
+    }}>
       <Stack direction="row" spacing={4}>
-        <Button sx={{color:'black'}} onClick={()=> {router.push("/pages/homepage")}}>Home</Button>
-        <Button sx={{color:'black'}} onClick={()=> {router.push("/pages/profile")}}>Profile</Button>
-        <Button sx={{color:'black'}} onClick={()=> {router.push("/pages/ranking")}}>Ranking</Button>
-        <Button sx={{color:'black'}} onClick={()=> {router.push("/pages/rules")}}>Rules</Button>
+        <Button
+          sx={{
+            color: 'white',
+            border: 'none', // Elimina cualquier borde visible
+            padding: '6px 16px', // Ajuste de padding para no dejar borde visible
+          }}
+          className="bg-transparent transition duration-300 ease-in-out hover:shadow-lg hover:shadow-white hover:bg-white hover:text-black"
+          onClick={() => { router.push("/pages/homepage") }}
+        >
+          Home
+        </Button>
+        <Button
+          sx={{
+            color: 'white',
+            border: 'none',
+            padding: '6px 16px',
+          }}
+          className="bg-transparent transition duration-300 ease-in-out hover:shadow-lg hover:shadow-white hover:bg-white hover:text-black"
+          onClick={() => { router.push("/pages/profile") }}
+        >
+          Profile
+        </Button>
+        <Button
+          sx={{
+            color: 'white',
+            border: 'none',
+            padding: '6px 16px',
+          }}
+          className="bg-transparent transition duration-300 ease-in-out hover:shadow-lg hover:shadow-white hover:bg-white hover:text-black"
+          onClick={() => { router.push("/pages/ranking") }}
+        >
+          Ranking
+        </Button>
+        <Button
+          sx={{
+            color: 'white',
+            border: 'none',
+            padding: '6px 16px',
+          }}
+          className="bg-transparent transition duration-300 ease-in-out hover:shadow-lg hover:shadow-white hover:bg-white hover:text-black"
+          onClick={() => { router.push("/pages/rules") }}
+        >
+          Rules
+        </Button>
       </Stack>
       <div className="absolute bottom-0 left-0 w-full h-[2px] bg-[#000000]" />
     </div>
--- frontend/src/app/pages/homepage/page.tsx
+++ frontend/src/app/pages/homepage/page.tsx
@@ -40,12 +40,12 @@ export default function Homepage() {
       >
         <div className="flex justify-center items-center mt-[8%] bg">
           <div className="flex flex-col items-center justify-center">
-            <div className="flex flex-col bg-opacity-90 w-[400px] h-[300px] rounded shadow-lg bg-slate-800">
+            <div className="flex flex-col items-center bg-opacity-90 w-[400px] h-[300px] rounded shadow-lg bg-slate-800">
               <h1 className="font-bold text-3xl text-center mb-6">Scrim 5vs5</h1>
               <p className="p-5">
                 A 5v5 match where the teams are chosen by two participants who decide to compete against each other. The winners of this match will earn 1 victory scrim and points. The winner is determined by the "BO1", "BO3" or "BO5".
               </p>
-              <Button className="m-5" href="/pages/creategame" variant="contained">
+              <Button className="w-[80%]" href="/pages/creategame" variant="contained">
                 Create game
               </Button>
             </div>
