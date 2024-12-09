043942decda42ab2a1038967b0ac0efb76013e97
Alejo Borracci <borraccialejo@hotmail.es>
2024-12-09 16:05:49 -0300

    port

---
+++ frontend/src/app/pages/history/page.tsx
@@ -0,0 +1,12 @@
+
+//CAMBIAR: Cuando se guarda un game, los teams se guardan por id y deberia guardar el nickname del jugador.
+
+//O buscar la manera de buscar el id de cada jugador antes de enviarselo al front.
+
+
+export default function History() {
+    return(
+        <>
+        </>
+    )
+}
