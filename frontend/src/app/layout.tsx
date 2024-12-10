cfad4b8c75655dc2c70ca008f812e96a7359c161
Alejo Borracci <borraccialejo@hotmail.es>
2024-12-10 15:06:43 -0300

    icon

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-
+import image from "@../../../public/logo.webp"
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -26,7 +26,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <title>Criters Ranks</title>
+        <link rel="icon"  href={image.src}/>
+        <title>Criters Rank</title>
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
